6bf7dbf26fedd4641a76ada2b6ffbbe08c30073a
Nguyen Le <nguyen.le@MacBook-Pro-36.local>
2019-02-22 12:37:32 -0800

    Update

---
+++ Section02_JS_Fundamentals/Lecture05_UsingConsole/app.js
@@ -0,0 +1,34 @@
+//Log to console
+console.log("Hello world");
+console.log(123);
+console.log(true);
+
+var greeting = 'Hello';
+console.log(greeting);
+
+console.log([1,2,3,4]);
+
+//Object
+console.log({a:1, b:2});
+
+//Print object as table
+console.table({a:1, b:2});
+
+//console error
+console.error("This is some errors");
+
+//Clear console
+console.clear();
+
+//Warn
+console.warn("This is a warning");
+
+//Time
+console.time("testForEach");
+    console.log("Test1");
+    console.log("Test2");
+console.timeEnd('testForEach');
+
+/*
+
+*/
---
+++ Section02_JS_Fundamentals/Lecture06_var_let_conts/app.js
@@ -0,0 +1,23 @@
+// var let const
+//block level scope
+
+var name = 'John Doe';
+console.log(name);
+
+//Re-asign variable 
+name = 'Steve Smith';
+console.log(name);
+
+//Init var
+var greeting;
+console.log(greeting);
+
+greeting = 'Hello';
+console.log(greeting);
+
+//
+var firstName = 'John';     //Camel case
+var first_name = 'Sara';    //Underscore
+var FirstName = 'Tom';      //Pascal case
+
+
