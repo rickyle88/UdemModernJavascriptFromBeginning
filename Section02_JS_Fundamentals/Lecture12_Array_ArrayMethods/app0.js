2a0f79a4568e1ecd4e17368aa7f8d3e091da79be
rickyle88 <bsbnguyen@gmail.com>
2019-04-15 08:03:44 -0700

    Update comments

--- Section02_JS_Fundamentals/Lecture12_Array_ArrayMethods/app0.js
+++ Section02_JS_Fundamentals/Lecture12_Array_ArrayMethods/app0.js
@@ -3,6 +3,7 @@ const numbers = [43,56,33,23,44,36,5];
 const numbers2 = new Array(22,45,33,76,54);
 const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
 const mixed = [22,'Hello', true, undefined, null, {a:1, b:1}, new Date()];
+
 let val;
 
 //Get array length
@@ -14,15 +15,16 @@ val = Array.isArray(numbers);
 
 
 //Get single value
-val = numbers[3];
-val = numbers[0];
+val = numbers[3];       //23
+val = numbers[0];       //43
 
 //Insert into array
 //array is 
 numbers[2] = 100;
 
 //Find index of value : count from 0
-val = numbers.indexOf(36);
+//const numbers = [43,56,100,23,44,36,5];
+val = numbers.indexOf(36);      //5
 
 //MUTATING ARRAYS
 //Add on to the end
@@ -36,14 +38,23 @@ numbers.pop();
 //Take off - remove from the front
 numbers.shift();
 
+////const numbers = [43,56,100,23,44,36,5];
 //Splice values
-numbers.splice(1,3);
+numbers.splice(1,3);    //New array: [43,44,36,5]
 
 //Reverse
-numbers.reverse();
+numbers.reverse();      //[43,44,36,5]
 
+//const numbers2 = new Array(22,45,33,76,54);
 //Concatenate array
 val = numbers.concat(numbers2);
+
+//[43,44,36,5,22,45,33,76,54]
+
+
+//const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+//Sorting arrays on array of strings
+val = fruit.sort(); //['Apple', 'Banana', 'Orange', 'Pear']
 
 //Sorting arrays
 val = numbers.sort(function(x,y){
@@ -64,8 +75,9 @@ function over40(num){
     return num > 40;
 }
 
-val = numbers.find(over40);
+val = numbers.find(under40);
 
 console.log(numbers);
+console.log("All numbers > 40");
 console.log(val);
 
