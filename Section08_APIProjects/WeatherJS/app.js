844b9fbc25073843ef79efeba019adfaa6527d5a
rickyle88 <bsbnguyen@gmail.com>
2019-05-06 23:28:38 -0700

    Update

--- Section08_APIProjects/WeatherJS/app.js
+++ Section08_APIProjects/WeatherJS/app.js
@@ -1,9 +1,38 @@
+// Init Storage
+const storage = new Storage();
+
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
 // Init Weather object
 // Get city, state from Local Storage
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+   
+
+    // Change location
+    weather.changeLocation(city, state);
+
+     // Set local storage
+     storage.setLocationData(city, state);
+
+    // Call getWeather() again
+    getWeather();
+
+    // Close modal using jQuery
+    $('#locModal').modal('hide');
+
+});
 
 // Change location
 // weather.changeLocation('Miami', 'FL');
+
+// Init UI
+const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -12,6 +41,7 @@ function getWeather() {
     weather.getWeather()
         .then(result => {
             console.log(result);
+            ui.paint(result);
         })
         .catch(err => console.log(err));
 }
---
+++ Section08_APIProjects/WeatherJS/ui.js
@@ -0,0 +1,26 @@
+class UI {
+    constructor() {
+        this.location = document.getElementById('w-location');
+        // this.country = document.getElementById('w-country');
+        this.description = document.getElementById('w-desc');
+        this.temp = document.getElementById('w-temp');
+        this.details = document.getElementById('w-details');
+        this.icon = document.getElementById('w-icon');
+        this.humidity = document.getElementById('w-humidity');
+        this.wind = document.getElementById('w-wind');
+        this.minTemp = document.getElementById('w-min-temp');
+        this.maxTemp = document.getElementById('w-max-temp');
+    }
+
+    paint(weather) {
+        this.location.textContent = weather.name;
+        // this.country.textContent = weather.sys.country;
+        this.description.textContent = weather.weather[0].description;
+        this.temp.textContent = weather.main.temp;
+        this.icon.setAttribute('src', `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+        this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`;
+        this.wind.textContent = `Wind Speed: ${weather.wind.speed}`
+        this.minTemp.textContent = `Lows: ${weather.main.temp_min}˚C`;
+        this.maxTemp.textContent = `Highs: ${weather.main.temp_max}˚C`;
+    }
+}
